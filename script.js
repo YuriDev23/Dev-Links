edb47b69490d7ca5e9ccf138f9d9348b6ff98fc4
YuriDev23 <ydossantos578@gmail.com>
2025-02-12 13:45:57 -0300

    Atualização de Layout
    Atualização de Layout

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ console.log("script.js carregado");
 
 function toggleMode() {
   const html = document.documentElement;
-  
+
   console.log("Modo trocado");  // Verificar se o evento de clique chama a função
 
-  if (html.classList.contains('light')) {
-    html.classList.remove('light');
-  } else {
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
     html.classList.add('light');
+    localStorage.setItem('theme', 'light'); // Armazena a preferência no localStorage
+  } else {
+    html.classList.remove('light');
+    html.classList.add('dark');
+    localStorage.setItem('theme', 'dark'); // Armazena a preferência no localStorage
   }
 }
+
+// Verifica a preferência do tema ao carregar a página
+window.onload = () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    html.classList.add(savedTheme);
+  } else {
+    html.classList.add('light'); // Define o tema claro como padrão
+  }
+};
 
